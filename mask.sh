c1ba525510154c0809f48c9821945c4be83a587d
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-01-30 10:59:50 +0530

    Update mask.sh

--- mask.sh
+++ mask.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#vallavantem script copy paste cheyyathe swantham kazhivu kond nthelum ondaakk By https://github.com/ArunKBiju/
 figlet Mask
 echo -e "www.github.com/ArunKBiju"
-# Function to check if the URL starts with http or https
+
 url_checker() {
     if [[ ! "${1}" =~ ^(http|https):// ]]; then
         echo -e "\e[31m [!] Invalid URL. Please use http or https.\e[0m"
@@ -9,10 +10,10 @@ url_checker() {
     fi
 }
 read -p "Input your Rebrand.ly API Key: " API_KEY
-# (Additional ASCII art lines...)
+
 echo -e "### Masking Domain ###"
 
-# Prompt for Phishing URL
+
 echo 'Paste Phishing URL here (with http or https): '
 echo -en "=>"
 read phish
@@ -21,33 +22,27 @@ sleep 1
 echo -e "\e[1;33m [*] Processing and modifying phishing URL...\e[0m"
 echo ""
 
-# Define the data as JSON
+
 data='{"destination": "'"$phish"'"}'
 
-# Shorten the URL using Rebrandly
 response=$(curl -s -X POST "https://api.rebrandly.com/v1/links" -H "apikey: $API_KEY" -H "Content-Type: application/json" -d "$data")
 
-# Extract the shortened URL using jq
 short=$(echo "$response" | jq -r '.shortUrl')
 
-# Extract the shortened URL
 shorter=${short}
 
 echo -e "\n### Masking Domain ###"
 
-# Prompt for Masking URL
 echo 'Domain to mask the Phishing URL (with http or https):'
 echo -en "=>"
 read mask
 url_checker $mask
 
-# Prompt for social engineering words
 echo -e '\nType Social Engineering words (like profile-name, free-followers).'
 echo -e "\e[1;33m [*] Don't use spaces, use '-' between social engineering words\e[0m"
 echo -en "=> "
 read words
 
-# Check for valid social engineering words
 if [[ -z "$words" || "$words" =~ " " ]]; then
     echo -e "\e[31m [!] Invalid or empty words. Please avoid spaces.\e[0m"
     echo -e "\e[1;33m [*] Generating Masked Phishing Link...\n"
